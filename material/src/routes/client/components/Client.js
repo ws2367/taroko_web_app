c156a8633c3591f22c93e3e9a9339f6bdee9f2b3
Wen Shaw <wenhsiang.shaw@gmail.com>
2020-07-19 23:35:15 -0700

    renamed tabs

--- material/src/routes/client/components/Client.js
+++ material/src/routes/client/components/Client.js
@@ -12,21 +12,6 @@ const HEADER = {
   "Authorization": "BEARER PS3eSI8zNXIa4m_bfc2P8Qh4XbQtgbX2bOz9qphHcKMinFmMtGpPkOtso1gKJDTvj0ZJmn9PzNEirnVPVcdlevTleq2mUuVPgsW0SnKR5GaQqrH-qmtwtTWkr77Mja0wzOATEevMPLuNWWh9e7aiP2Tqkw8Hc69BA41nB2ozrhg"
 };
 
-const TabContent2 = ( {notes} ) => (
-  <div className="container-fluid no-breadcrumb container-mw-md chapter">
-    <article className="article">
-      <QueueAnim type="bottom" className="ui-animate">
-        <div key="1" className="mb-3"> <Note notes={notes}/> </div>
-      </QueueAnim>
-    </article>
-  </div>
-)
-
-const TabContent3 = () => (
-  <div className="container-fluid pt-3">
-    <TaskList />
-  </div>
-)
 
 class Client extends React.Component {
   constructor(props) {
@@ -39,7 +24,7 @@ class Client extends React.Component {
       notes: [],
       todos: [],
       clientId: clientId,
-      name: "",
+      name: null,
       error: null,
       isLoaded: false,
       tabIndex: 0,
@@ -110,13 +95,30 @@ class Client extends React.Component {
 
   render() {
 
-    const { clientId, name, tabIndex, isLoaded, handlers } = this.state;
+    const { clientId, name, tabIndex, isLoaded, handlers, notes } = this.state;
+
+
+    const NoteTab = () => (
+      <div className="container-fluid no-breadcrumb container-mw-md chapter">
+        <article className="article">
+          <QueueAnim type="bottom" className="ui-animate">
+            <div key="1" className="mb-3"> <Note notes={notes}/> </div>
+          </QueueAnim>
+        </article>
+      </div>
+    )
+
+    const TaskTab = () => (
+      <div className="container-fluid pt-3">
+        <TaskList />
+      </div>
+    )
 
     const ClientWithTabs = () => (
       <section className="page-with-tabs">
         <QueueAnim type="bottom" className="ui-animate">
           <div key="1">
-            <div className="page-header-title"> {name} </div>
+            <div className="page-header-title"> {name || "未命名"} </div>
           </div>
 
           <div key="2">
@@ -133,8 +135,8 @@ class Client extends React.Component {
                 clientId={clientId}
                 handlers={handlers}
               />
-              <TabContent2 notes={this.state.notes}/>
-              <TabContent3 />
+              <NoteTab />
+              <TaskTab />
             </SwipeableViews>
           </div>
         </QueueAnim>
